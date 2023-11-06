f58a62c0bbf98ab8ee27e7a1b251ae1e44b75f17
Matheus <matheus.lunkes.corp@gmail.com>
2023-11-06 20:44:06 -0300

    Aula 1 - Console.log

---
+++ Módulo 1 - JavaScript Básico para Iniciantes/Aula 1 - Console.log/index.js
@@ -0,0 +1,33 @@
+/* AULA 1 - Console.log*/
+
+/* A função console.log pode receber valores como variaveis, funções, objetos, textos e etc. 
+
+Console.log é usado para exibir valores.
+*/
+
+
+var car = 'Fusion';
+
+let fruta = 'Laranja';
+
+const data = '20/10/1997';
+
+const pessoa = {Nome: 'Matheus', Idade: 24}
+
+const soma = function(num){
+    let resultado = num + num;
+    return resultado;
+}
+
+
+console.log(soma(2)); //Funções
+console.log(pessoa) //Objetos 
+console.log(data); //constantes
+console.log(car, fruta); //Variáveis com var e let
+console.log(10); //Numeros
+console.log('Olá mundo'); //Texto
+
+
+
+
+
