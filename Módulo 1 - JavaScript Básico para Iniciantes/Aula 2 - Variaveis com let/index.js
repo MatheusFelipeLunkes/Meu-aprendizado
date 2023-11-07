13d568c259d7ec0f2801cecb1aa09c7a465f52ec
Matheus <matheus.lunkes.corp@gmail.com>
2023-11-06 22:01:25 -0300

    Aula 2 - Variáveis com LET

---
+++ Módulo 1 - JavaScript Básico para Iniciantes/Aula 2 - Variaveis com let/index.js
@@ -0,0 +1,41 @@
+
+/* Aula 2 - Variaveis com LET */
+
+/* Oque são váriaveis? 
+    Variaveis são espaços na memória destinados a dados. Podendo ou não serem alterados durante a execução de um algoritmo.
+
+    Variáveis podem ter seus valores reatribuidos.
+*/
+
+//Exemplo:
+let nome = "Matheus"; 
+
+console.log(`${nome} nasceu em 1999.`);
+console.log(`Em 2019 ${nome} conheceu Márcia.`);
+console.log(`${nome} casou-se com Márcia em 2023.`);
+
+
+/* Variáveis podem ter seus valores reatribuidos mas não podem ser redeclarados. */
+nome = 'Matheus Felipe';
+console.log(nome);
+
+
+/* REGRAS PARA VARIAVEIS: 
+
+    -Não podemos criar variáveis com palavras reservadas, como por exemplo, var, let, function etc;
+
+    -Não podemos começar o nome de  variáveis com números;
+
+    -Não podedm conter espaços ou traços;
+
+    -Utilizamos sempre camelCase;
+
+    -Variáveis são Case-sesitive, oque quer dizer que letras maiusculas e letra minusculas são diferentes na declaração de variaveis.
+
+    -Variaveis precisam ter nomes significativos, exemplo:
+*/
+let nomeCompleto = 'Matheus Felipe Lunkes';
+
+
+
+
