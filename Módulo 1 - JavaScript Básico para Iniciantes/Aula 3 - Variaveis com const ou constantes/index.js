012f516b616635eb1ad5660a8bddf3988a20d779
Matheus <matheus.lunkes.corp@gmail.com>
2023-11-07 20:30:06 -0300

    Aula 3 - Variaveis com const ou constantes

---
+++ Módulo 1 - JavaScript Básico para Iniciantes/Aula 3 - Variaveis com const ou constantes/index.js
@@ -0,0 +1,32 @@
+/* AULA 3 - Variaveis com const ou constantes */
+
+/* Oque são variaveis constantes? 
+    Uma variavel constante é tambem uma variavel para armazenar dados, mas com uma pequena regra, os dados armazenados são imutaveis/fixos, ou seja, não podemos reatribuir o valor das variaveis constantes.
+        Exemplo:
+*/
+
+const carro = "Celta";
+
+const numeroSorte = 64;
+
+const nomeCompleto = "Matheus Felipe Lunkes";
+
+console.log(carro, numeroSorte, nomeCompleto);
+
+
+/* Regras para constantes:
+
+    Não podemos criar constantes com palavras reservadas da linguagem;
+
+    Constantes precisam ter nomes significativos;
+
+    Não podemos começar o nome de uma constante com número;
+
+    Não podem conter espaços ou traços;
+
+    Utiliza-se sempre camelCase;
+
+    Não podemos modificar ou reatribuir o valor de uma constante;
+
+*/
+
