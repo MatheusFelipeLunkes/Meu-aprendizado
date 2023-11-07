f89b08168b8fc4fa2d106d4a0e0b148f25707e84
Matheus <matheus.lunkes.corp@gmail.com>
2023-11-06 21:04:06 -0300

    Exercicio sobre Console.log, referente a Aula 1

---
+++ Módulo 1 - JavaScript Básico para Iniciantes/Aula 1 - Console.log/exercicios.js
@@ -0,0 +1,14 @@
+/* Exercicios com console.log */
+
+/* 
+    Você deve imprimir a seguinte frase usando console.log: 
+    Meu nome é "NOME". Estou aprendendo Javascript ás 10 horas da manhã.
+
+*/
+
+
+let nome = 'Matheus';
+
+const horas = 21;
+
+console.log(`Meu nome é "${nome}". Estou aprendendo Javascript às ${horas} da noite.`);
