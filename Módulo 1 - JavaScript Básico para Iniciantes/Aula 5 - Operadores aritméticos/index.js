74ea212a7f3c5ac5315b1b3f9c682acbf2ec9c19
Matheus <matheus.lunkes.corp@gmail.com>
2023-11-07 22:01:34 -0300

    Aula 5 - Operadores aritméticos

---
+++ Módulo 1 - JavaScript Básico para Iniciantes/Aula 5 - Operadores aritméticos/index.js
@@ -0,0 +1,36 @@
+/* Aula 5 - Operadores Aritméticos */
+
+/* 
+    Operadores aritméticos básicos:
+
+        + = Adição / concatenação;
+        - = Subtração;
+        * = Multiplicação;
+        / = Divisão;
+        % = Resto da divisão;
+        ** = Potenciação;
+        ++ = Incremento;
+        -- = Decremento 
+    Exemplos:
+*/
+
+
+const num1 = 12;
+
+const num2 = 6;
+
+const numero = '1';
+
+let num3 = 3;
+
+console.log(num1 + num2); // Adição
+console.log(numero + num2); // Concatenação
+console.log(num1 - num2); // Subtração
+console.log(num1 / num2); // Divisão
+console.log(num1 % num2); // Resto da divisão
+console.log(num1 ** num2); // Potenciação
+console.log(++num3); // Incremento
+console.log(--num3); // Decremento
+
+
+
