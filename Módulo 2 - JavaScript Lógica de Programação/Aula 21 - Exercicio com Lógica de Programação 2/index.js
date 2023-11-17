59c75962a9859f6e540f456d6ea8b6e0e5bbe20f
Matheus <matheus.lunkes.corp@gmail.com>
2023-11-16 22:12:39 -0300

    Aula 21 - Exercicio com Lógica de programação 2

---
+++ Módulo 2 - JavaScript Lógica de Programação/Aula 21 - Exercicio com Lógica de Programação 2/index.js
@@ -0,0 +1,10 @@
+/* Aula 21 - Exercicio com Lógica de Programação 2  */
+
+/*
+    Escreva uma Função chamada ePaisagem que recebe dois argumentos de uma imagem (Number).
+    Retorne true se a imagem estiver no modo paisagem.
+*/
+
+const ePaisagem = (b, h) => b >= h;
+
+console.log(ePaisagem(1080, 1920));
