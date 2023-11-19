681ab48ec7df0aa67685d7238f3263e64312fb1c
Matheus <matheus.lunkes.corp@gmail.com>
2023-11-18 23:58:48 -0300

    Aula 25 - SetInterval e setTimeout

---
+++ Módulo 2 - JavaScript Lógica de Programação/Aula 25 - SetInterval e setTimeout/index.js
@@ -0,0 +1,20 @@
+/* Aula 25 - SetInterval e setTimeout */
+
+/* A função setInterval, ela seta um parametro de tempo em milessegundos para a execução do codigo dentro do bloco, ela nunca terminará se não usarmos o clearInterval */
+
+
+function mostraHora () {
+    let date = new Date();
+    return date.toLocaleTimeString('pt-BR', {
+        hours12: false
+    })
+}
+ 
+const timer = setInterval(() => {
+    console.log(mostraHora());
+}, 1000);
+
+
+setTimeout (() => {
+    clearInterval(timer);
+}, 10000);
