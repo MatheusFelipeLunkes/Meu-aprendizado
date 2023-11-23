09d45422ddb91d2ed0fc6340752d994f421586ef
Matheus <matheus.lunkes.corp@gmail.com>
2023-11-22 22:47:52 -0300

    Refatorando codigo, da Aula 27 - Lista de tarefas

--- Módulo 2 - JavaScript Lógica de Programação/Aula 27 - Criando uma lista de tarefas/js/index.js
+++ Módulo 2 - JavaScript Lógica de Programação/Aula 27 - Criando uma lista de tarefas/js/index.js
@@ -4,42 +4,80 @@
     *** Toda vez que capturarmos um input, tratar os dados mesmo que sejam vazios, não podemos receber dados vazios. 
 */
 
-const inputTarefa = document.querySelector('.text-tarefa');
-const inputAdicionaTarefa = document.querySelector('.adiciona-tarefa');
-const lista = document.querySelector('.lista');
+//REFATORANDO...
 
+const inputTarefa = document.querySelector('.input-tarefa');
+const btnAddTarefa = document.querySelector('.btn-add-tarefa');
+const tarefas = document.querySelector('.tarefas');
 
-inputAdicionaTarefa.addEventListener('click', () => {
-    if(!inputTarefa.value) {
-        console.log('Apenas inputs Cheios são aceitos')
-    }else {
-        adicionaTarefa(inputTarefa);
+btnAddTarefa.addEventListener('click', () => {
+    if(!inputTarefa.value) return;
+    criaTarefa(inputTarefa.value);
+})
+
+inputTarefa.addEventListener('keypress', function(e) {
+    if(e.keyCode === 13) {
+
+        if(!inputTarefa.value) return;
+        criaTarefa(inputTarefa.value);
+        limpaInput();
     }
-});
+})
 
-function adicionaTarefa(texto) {
-    const text = texto.value;
-    criaLi();
-    criaButton();
-    const li = criaLi();
-    const button = criaButton();
-    li.innerText = text;
-    li.appendChild(button);
-    lista.appendChild(li);
-
-    button.addEventListener('click', () => {
-        lista.removeChild(li);
-    })
-    
+function limpaInput () {
+    inputTarefa.value = '';
+    inputTarefa.focus();
 }
-function criaLi() {
+
+
+function criaLi () {
     const li = document.createElement('li');
-    return li; 
+    return li;
 }
 
-function criaButton() {
-    const button = document.createElement('button'); 
-    button.innerText = 'Remover Tarefa';
-    button.classList.add('remove-tarefa');
-    return button;
+function criaTarefa(textoInput) {
+    const li = criaLi();
+    li.innerText = textoInput;
+    tarefas.appendChild(li);
+    limpaInput();
+    criaBotaoApagar(li);
+    salvarTarefas();
+}
+
+
+function criaBotaoApagar (li) {
+    li.innerText += ' ';
+    const botaoApagar = document.createElement('button');
+    botaoApagar.innerText = 'Apagar';
+    botaoApagar.setAttribute('class', 'apagar');
+    li.appendChild(botaoApagar);
+}
+document.addEventListener('click', function(e) {
+    const element = e.target;
+
+    if(element.classList.contains('apagar')) {
+        element.parentElement.remove();
+        salvarTarefas();
+    }
+})
+
+function salvarTarefas() {
+   const liTarefas = tarefas.querySelectorAll('li');
+   const listaDeTarefas = [];
+
+   for (let tarefa of liTarefas) {
+    let tarefaTexto = tarefa.innerText;
+    tarefaTexto = tarefaTexto.replace('Apagar', '').trim();
+    listaDeTarefas.push(tarefaTexto);
+} 
+    const tarefasJSON = JSON.stringify(listaDeTarefas); //criando uma string em formato JSON
+    localStorage.setItem('tarefas', tarefasJSON);
+}
+
+function adicionaTarefasSalvas () {
+    const tarefas = localStorage.getItem('tarefas');
+    const listaDeTarefas = JSON.parse(tarefas); // Convertendo um formato JSON, de volta ao seu estado natural.
+    for (let tarefa of listaDeTarefas) {
+        criaTarefa(tarefa);
+    }
 }
