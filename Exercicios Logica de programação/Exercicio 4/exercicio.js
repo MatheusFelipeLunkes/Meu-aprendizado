5699cb1b9c66c02bcae90603cd6fb2751f587593
Matheus <matheus.lunkes.corp@gmail.com>
2023-11-27 21:43:12 -0300

    Exercicio 4 - Logica de Programação

---
+++ Exercicios Logica de programação/Exercicio 4/exercicio.js
@@ -0,0 +1,22 @@
+/* EXERCICIO 4 - Crie uma função que receba length e width e encontre o perímetro de um retângulo. */
+
+/*
+findPerimeter(6, 7) ➞ 26
+
+findPerimeter(20, 10) ➞ 60
+
+findPerimeter(2, 9) ➞ 22
+Notas:
+Não se esqueça do return resultado.
+*/
+
+
+function findPerimeter(length, width) {
+    const resultPerimeter = (length * 2) + (width * 2);
+    return console.log(resultPerimeter);
+}
+findPerimeter(6, 7)
+
+findPerimeter(20, 10) 
+
+findPerimeter(2, 9)
